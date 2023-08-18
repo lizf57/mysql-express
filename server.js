f12aaa2e0fda7a69f4339eaabb882771853f4bd7
mona lisa <lizfriedman5@gmail.com>
2023-08-17 19:47:03 -0500

    install mysql2, add node

---
+++ server.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database: 'pokemon_db',
+});
+
